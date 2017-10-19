5cd92e41965ee306dbb9c3b1788337a1b2d8f5ae
FurkanT <furkan.tukek@hotmail.com>
2017-10-19 18:32:47 +0300

    Add files via upload

--- Exercise/foursquare/forms.py
+++ Exercise/foursquare/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
 from foursquare.models import LocationSearch
+from django.forms.widgets import HiddenInput
+
 
 class LocationForm(ModelForm):
     class Meta:
         model = LocationSearch
-        fields = ['food', 'location']
+        fields = ['food', 'location', 'offset']
+        widgets = {'offset': HiddenInput()}
